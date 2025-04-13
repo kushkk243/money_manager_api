9685e36d711c7c20be0ff3bc7444ca331ea4a3c5
kushkk <kushkushwaha52@gmail.com>
2025-04-13 10:38:31 +0530

    cors and monthly totals added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends, HTTPException, Query
+from fastapi.middleware.cors import CORSMiddleware
 from database import Payment_Database
 from sqlmodel import create_engine, select, Session, SQLModel
 from typing import Annotated
@@ -26,8 +27,13 @@ def on_startup():
 app = FastAPI( on_startup=on_startup())
 monthly_budget = 10000
 # Create the database and tables
-
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 # Create a test payment entry
 @app.get("/")
 async def root():
@@ -128,10 +134,26 @@ async def get_payments_by_month(year: int, month: int, session: SessionDep):
 async def get_pie_data(session: SessionDep, month: int = datetime.now().month, year: int = datetime.now().year):
     start_date = datetime(year, month, 1)
     end_date = datetime(year, month + 1, 1) if month < 12 else datetime(year + 1, 1, 1)
-    payments = session.exec(select(Payment_Database).where(Payment_Database.timestamp >= month, Payment_Database.timestamp<end_date)).all()
+    payments = session.exec(select(Payment_Database).where(Payment_Database.timestamp >= start_date, Payment_Database.timestamp<end_date)).all()
     categories = {}
     for payment in payments:
         if payment.category not in categories:
             categories[payment.category] = 0
         categories[payment.category] += payment.amount
     return categories
+
+@app.get("/payments/totals/{year}/{month}")
+async def get_total_payments_by_month(year: int, month: int, session: SessionDep):
+    start_date = datetime(year, month, 1)
+    if month == 12:
+        end_date = datetime(year + 1, 1, 1)
+    else:
+        end_date = datetime(year, month + 1, 1)
+    payments = session.exec(select(Payment_Database).where(Payment_Database.timestamp >= start_date, Payment_Database.timestamp < end_date)).all()
+    total = sum([payment.amount for payment in payments])
+    categories = {}
+    for payment in payments:
+        if payment.category not in categories:
+            categories[payment.category] = 0
+        categories[payment.category] += payment.amount
+    return {"total": total, "category_wise": categories}
